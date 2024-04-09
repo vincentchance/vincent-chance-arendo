9cb25f621d9394a15cdeb012463a1fdf8c0a5b87
Vincent Chance <80244284+vincentchance@users.noreply.github.com>
2024-04-09 07:23:02 +0700

    update routes for order_item

---
+++ routes/orderRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+	createOrderItem,
+	getOrderItem,
+	getOrderItemById
+} from "../controller/order-controller.js";
+
+const router = express.Router();
+
+router.get('/order_item', getOrderItem);
+router.get('/order_item/:id', getOrderItemById);
+router.post('/order_item', createOrderItem);
+
+export default router;
