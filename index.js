c1b72ded3af88f3dcd9ab6af80faf3fb7afed5a5
Vincent Chance <80244284+vincentchance@users.noreply.github.com>
2024-04-09 07:27:10 +0700

    Update index.js
    add new routes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import CustomerRoutes from "./routes/CustomerRoutes.js";
-import ProductRoutes from "./routes/ProductRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(CustomerRoutes);
 app.use(ProductRoutes);
+app.use(orderRoutes);
+
 
 app.listen(8000,() => console.log('Server up and running...'));
