28bc49bedb9b404393552ffdd4e50ff30155e0e4
Vincent Chance <80244284+vincentchance@users.noreply.github.com>
2024-04-09 07:17:48 +0700

    Create order-controller.js
    update createorder controller for test number 2 backend

---
+++ controller/order-controller.js
@@ -0,0 +1,23 @@
+import OrderItem from "../models/order_item.js"
+
+export const createOrderItem = async(req, res) => {
+	try {
+		const {quantity, price, customerId, productId, discount } = req.body;
+		const totalPrice = price - discount; //buat variabel total harga
+		const orderItem = await OrderItem.create({quantity, price: totalPrice, customerId});
+		await orderItem.addProduct(productId);
+		
+		res.status(201).json({orderItem});
+	} catch(error){
+	console.log(error.message);
+	}
+}
+
+export const getOrderItem = async(req, res) =>{
+	try {
+		const response = await OrderItem.findAll();
+		res.status(200).json(response);
+	} catch(error){
+	console.log(error.message);
+	}
+}
