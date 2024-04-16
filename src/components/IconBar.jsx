a5041823075aef840cd242eb957225c28407a793
Vincent Chance <80244284+vincentchance@users.noreply.github.com>
2024-04-16 16:51:42 +0700

    Add files via upload

---
+++ src/components/IconBar.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {IconButton, Menu, MenuItem} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+
+const IconBar = () => {
+    const [anchor, setAnchor] = useState(null);
+
+    const handleClick = (event) =>{
+        setAnchor(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchor(null);
+    }
+  return (
+    <div>
+        <IconButton
+            onclick={handleClick}
+            aria-controls="menu"
+            aria-haspopup="true"
+            ><MenuIcon />
+            </IconButton>
+        <Menu
+        id="menu"
+        anchor={anchor}
+        open={Boolean(anchor)}
+        onClose={handleClose}>
+            <MenuItem onclick={handleClose}>Edit</MenuItem>
+            <MenuItem onclick={handleClose}>Delete</MenuItem>
+        </Menu>
+    </div>
+  )
+}
+
+export default IconBar;
