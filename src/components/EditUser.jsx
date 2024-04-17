dcb037214513363e9d011fa38493d22f08e3f0b3
Vincent Chance <80244284+vincentchance@users.noreply.github.com>
2024-04-17 22:49:21 +0700

    Create EditUser.jsx

---
+++ src/components/EditUser.jsx
@@ -0,0 +1,94 @@
+import React, {useState ,useEffect} from 'react';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+
+
+const EditUser = () => {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const navigate = useNavigate();
+    const {id} = useParams();
+
+    useEffect(()=>{
+        getUserById();
+    },[]);
+    
+    const updateUser = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.patch(`http://localhost:5000/customers/${id}`,{
+                name,
+                phone,
+                email,
+                address
+            })
+            navigate("/");
+        } catch (error){
+            console.log(error);
+        }
+    };
+
+    const getUserById = async() => {
+        const response = await axios.get(`http://localhost:5000/customers/${id}`);
+        setName(response.data.name);
+        setPhone(response.data.phone);
+        setEmail(response.data.email);
+        setAddress(response.data.address);
+    }
+  return (
+
+    <div className="columns mt-5 is-centered">
+        <div className="column is-half">
+            <form onSubmit={updateUser}>
+                <div className="field">
+                    <label className="label">Name</label>
+                        <div className="control">
+                            <input type="text" 
+                            className="input" 
+                            value={name}
+                            onChange={(e)=> setName(e.target.value)}
+                            placeholder="Name"/>
+                        </div>
+                    </div>
+                <div className="field">
+                    <label className="label">Phone Number</label>
+                        <div className="control">
+                            <input type="text" 
+                            className="input" 
+                            value={phone}
+                            onChange={(e)=> setPhone(e.target.value)}
+                            placeholder='Phone Number'/>
+                        </div> 
+                    </div> 
+                    <div className="field">
+                    <label className="label">Email</label>
+                        <div className="control">
+                            <input type="text" 
+                            className="input" 
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
+                            placeholder='Email'/> 
+                        </div> 
+                    </div>
+                    <div className="field">
+                    <label className="label">Address</label>
+                        <div className="control">
+                            <input type="text" 
+                            className="input"
+                            value={address}
+                            onChange={(e)=> setAddress(e.target.value)}
+                            placeholder='Address'/> 
+                        </div> 
+                    </div>
+                    <div className="field">
+                        <button type='submit' className="button is-success">Update</button>
+                </div>                  
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default EditUser;
