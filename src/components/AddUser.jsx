d4ba980ea459802183b425d9d36613b218aea2c7
Vincent Chance <80244284+vincentchance@users.noreply.github.com>
2024-04-17 21:47:08 +0700

    Update AddUser.jsx

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,32 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const AddUser = () => {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const navigate = useNavigate();
+    
+    const saveUser = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/customers',{
+                name,
+                phone,
+                email,
+                address
+            })
+            navigate("/");
+        } catch (error){
+            console.log(error);
+        }
+    }
   return (
+
     <div className="columns mt-5 is-centered">
         <div className="column is-half">
-            <form action="">
+            <form onSubmit={saveUser}>
                 <div className="field">
                     <label className="label">Name</label>
                         <div className="control">
-                            <input type="text" className="input" placeholder='Name'/>
+                            <input type="text" 
+                            className="input" 
+                            value={name}
+                            onChange={(e)=> setName(e.target.value)}
+                            placeholder="Name"/>
                         </div>
                     </div>
                 <div className="field">
                     <label className="label">Phone Number</label>
                         <div className="control">
-                            <input type="text" className="input" placeholder='Phone Number'/>
+                            <input type="text" 
+                            className="input" 
+                            value={phone}
+                            onChange={(e)=> setPhone(e.target.value)}
+                            placeholder='Phone Number'/>
                         </div> 
                     </div> 
                     <div className="field">
                     <label className="label">Email</label>
                         <div className="control">
-                            <input type="text" className="input" placeholder='Email'/> 
+                            <input type="text" 
+                            className="input" 
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
+                            placeholder='Email'/> 
                         </div> 
                     </div>
                     <div className="field">
                     <label className="label">Address</label>
                         <div className="control">
-                            <input type="text" className="input" placeholder='Address'/> 
+                            <input type="text" 
+                            className="input"
+                            value={address}
+                            onChange={(e)=> setAddress(e.target.value)}
+                            placeholder='Address'/> 
                         </div> 
                     </div>
                     <div className="field">
