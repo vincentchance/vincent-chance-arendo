a22be72c60a947acb88a75b5ccca8924bf16388a
Vincent Chance <80244284+vincentchance@users.noreply.github.com>
2024-04-17 22:58:16 +0700

    Update UserList.jsx

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -13,6 +13,15 @@ const getCustomers = async () =>{
     const response = await axios.get('http://localhost:5000/customers');
     setCustomers(response.data);
 }
+
+const deleteCustomers = async(id) => {
+    try {
+        await axios.delete(`http://localhost:5000/customers/${id}`);
+        getCustomers();
+    } catch (error) {
+        console.log(error);
+    }
+}
   return (
     <div className="columns mt-5 is-centered">
         <div className="column is-half">
@@ -35,7 +44,7 @@ const getCustomers = async () =>{
                         <td>{customer.email}</td>
                         <td>{customer.address}</td>
                         <td><Link to={`edit/${customer.id}`}className='button is-small is-info'>Edit</Link>
-                        <button className='button is-small is-danger'>Delete</button>
+                        <button onClick={() => deleteCustomers(customer.id)} className='button is-small is-danger'>Delete</button>
                         </td>
                     </tr>
                 ))}
